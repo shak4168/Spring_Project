d1a2ac3e6142bf7cf54c57bc64974071fe81bfb2
shak4168 <44919654+shak4168@users.noreply.github.com>
2026-02-06 22:07:51 +0900

    파일업로드 오류 수정

--- src/main/java/com/project/web/config/WebMvcConfig.java
+++ src/main/java/com/project/web/config/WebMvcConfig.java
@@ -8,13 +8,14 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
+    // 기본값은 윈도우용으로 두되, 주입받을 수 있게 설정
     @Value("${file.dir:C:/web-project/images/}")
     private String fileDir;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        // /images/** 로 시작하는 요청은 로컬 폴더에서 찾는다.
+        // file:/// 대신 file: 을 사용하세요 (리눅스 호환성 UP)
         registry.addResourceHandler("/images/**")
-                .addResourceLocations("file:///" + fileDir);
+                .addResourceLocations("file:" + fileDir);
     }
 }
